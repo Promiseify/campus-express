7fd49e1621e87f99c2daacdea861775a602abb89
xinyu <yu.life@qq.com>
2024-03-27 14:03:55 +0800

    增加plugin，包括模态框和页面跳转

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 import App from './App'
 import Vue from 'vue'
 import uView from "uview-ui";
+import plugins from './plugins';
+
 import './uni.promisify.adaptor'
-Vue.config.productionTip = false
+
 Vue.use(uView);
+Vue.use(plugins)
+Vue.config.productionTip = false
+
 App.mpType = 'app'
+
 const app = new Vue({
   ...App
 })
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <view :style="{ 'height': windowHeight }" style="width: 100%">
-    <div class="bgImgCover">
-      <img src="@/static/images/common/cover.jpg" alt="">
+  <view>
+    <div class="banner">
+      <img src="@/static/images/common/cover.jpg" alt="封面图片">
     </div>
-    <form>
+    <uni-form ref="form" :modalValue="queryParams">
       <view class="cu-form-group margin-top">
         <view class="title">账户</view>
         <input placeholder="请输入用户名" v-model="username"></input>
@@ -19,11 +19,11 @@
         </picker>
       </view>
 
-      <view class="padding flex flex-direction" @tap="login()">
-        <button class="cu-btn bg-red margin-tb-sm lg">登录</button>
+      <view class="padding flex flex-direction" @tap="handleLogin()">
+        <button class="cu-btn bg-blue margin-tb-sm lg">登录</button>
       </view>
-      <button class="cu-btn  margin-tb-sm" @tap="toregister()">没有账号注册一个</button>
-    </form>
+      <button class="cu-btn margin-tb-sm" @tap="toregister()">没有账号注册一个</button>
+    </uni-form>
 
   </view>
 </template>
@@ -32,6 +32,11 @@
 export default {
   data() {
     return {
+      queryParams: {
+        username: undefined,
+        password: undefined,
+        role: undefined
+      },
       windowHeight: "200px",
       msg: '',
       username: '',
@@ -57,42 +62,42 @@ export default {
       this.roleId = this.roleList[index].id
       this.roleName = this.roleList[index].name
     },
-    login: function () {
-      var that = this
+    handleLogin: function () {
+      // var that = this
       if (this.username.length == 0 || this.password.length == 0) {
         this.vusui.alert('请输入用户名和密码')
         return false;
       }
-      this.http.post('/openapi/system/loginNocode', {
-        username: this.username,
-        password: this.password,
-        roleId: this.roleId
-      }).then((res) => {
+      // this.http.post('/openapi/system/loginNocode', {
+      //   username: this.username,
+      //   password: this.password,
+      //   roleId: this.roleId
+      // }).then((res) => {
 
-        if (res.code != 0) {
-          this.vusui.alert(res.msg)
-          return false;
-        }
-        //登陆成功后获取用户信息，放入storage
-        uni.setStorageSync('token', res.data.userId)
-        uni.setStorageSync('userId', res.data.userId)
-        uni.setStorageSync('roleId', that.roleId)
-        uni.setStorageSync('deptId', res.data.dept.deptId)
-        uni.setStorageSync('deptName', res.data.dept.deptName)
-        uni.setStorageSync('userName', res.data.userName)
-        uni.setStorageSync('phonenumber', res.data.phonenumber)
-        uni.setStorageSync('avatar', res.data.avatar)
-        uni.switchTab({
-          url: "../index/index"
-        })
-      })
+      //   if (res.code != 0) {
+      //     this.vusui.alert(res.msg)
+      //     return false;
+      //   }
+      //   //登陆成功后获取用户信息，放入storage
+      //   uni.setStorageSync('token', res.data.userId)
+      //   uni.setStorageSync('userId', res.data.userId)
+      //   uni.setStorageSync('roleId', that.roleId)
+      //   uni.setStorageSync('deptId', res.data.dept.deptId)
+      //   uni.setStorageSync('deptName', res.data.dept.deptName)
+      //   uni.setStorageSync('userName', res.data.userName)
+      //   uni.setStorageSync('phonenumber', res.data.phonenumber)
+      //   uni.setStorageSync('avatar', res.data.avatar)
+      //   uni.switchTab({
+      //     url: "../index/index"
+      //   })
+      // })
     }
   }
 }
 </script>
 
 <style scoped>
-.bgImgCover {
+.banner {
   height: 400upx;
   overflow: hidden;
 
